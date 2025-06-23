1d1db2a427ac05ff06efce83ac7fd49e6f97ee5c
willeyzhou1 <willzhou000@gmail.com>
2025-06-23 10:57:55 -0700

    Basic espn scraping done

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from bs4 import BeautifulSoup
+import urllib.request
+from data_functions.get_stats import get_stats
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def printMsg():
+    return get_stats()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ data_functions/get_stats.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+def get_stats():
+    try:
+        url = "https://www.basketball-reference.com/leagues/NBA_2025_per_game.html"
+        html = urllib.request.urlopen(url)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        table = soup.find("table", {'id': 'per_game_stats'})
+        if not table:
+            return "Table not found"
+
+        return table.prettify()
+    except Exception as e:
+        return f"An error occurred: {e}"
